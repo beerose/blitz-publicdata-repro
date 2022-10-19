73a3cf9318f920941c2801f6bb8144e3b8e29b93
beerose <alexsandra.sikora@gmail.com>
2022-10-19 16:02:31 +0800

    Update types.ts file

--- types.ts
+++ types.ts
@@ -1,14 +1,17 @@
 import { SimpleRolesIsAuthorized } from "@blitzjs/auth"
-import { User } from "db"
 
-export type Role = "ADMIN" | "USER"
+// simplified it a bit
+export type Role = 'MembershipRole' | 'GlobalRole'
 
 declare module "@blitzjs/auth" {
   export interface Session {
     isAuthorized: SimpleRolesIsAuthorized<Role>
     PublicData: {
-      userId: User["id"]
-      role: Role
+      userId: string
+      isAdmin: boolean
+      role?: Role
+      orgRole?: Role
+      orgId?: string
     }
   }
 }
